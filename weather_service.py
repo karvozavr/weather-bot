a94ddffffa18d96f2e3d4f7730794f28e7f369ce
karvozavr <karvozavr@gmail.com>
2020-04-08 20:24:46 +0300

    Implement weather service

---
+++ weather_service.py
@@ -0,0 +1,54 @@
+import logging
+import os
+
+import aiohttp.client
+from aiogram.types import Location
+import urllib
+
+
+WEATHER_SERVICE_API_KEY = os.getenv('WEATHER_SERVICE_API_KEY')
+
+
+class WeatherServiceException:
+    pass
+
+
+class WeatherInfo:
+
+    def __init__(self, temperature, status, is_kelvin=True):
+        self.temperature = kelvin_to_celsius(
+            temperature) if is_kelvin else temperature
+        self.status = status
+
+    def __str__(self):
+        return f'{self.status}, температура {self.temperature}'
+
+
+async def get_weather_for_city(city_name: str) -> WeatherInfo:
+    return await make_weather_service_query(get_city_query_url(city_name))
+
+
+async def get_weather_for_location(location: Location) -> WeatherInfo:
+    return await make_weather_service_query(get_location_query_url(location))
+
+
+def get_city_query_url(city_name: str):
+    return f'http://api.openweathermap.org/data/2.5/weather?q={urllib.parse.quote(city_name)}&appid={WEATHER_SERVICE_API_KEY}&lang=ru'
+
+
+def get_location_query_url(location: Location):
+    return f'http://api.openweathermap.org/data/2.5/weather?lat={location.latitude}&lon={location.longitude}&appid={WEATHER_SERVICE_API_KEY}&lang=ru'
+
+
+async def make_weather_service_query(url: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            print(resp.status)
+            print(await resp.text())
+
+    raise WeatherServiceException()
+
+
+def kelvin_to_celsius(degrees):
+    KELVIN_0 = 273.15
+    return degrees - KELVIN_0
