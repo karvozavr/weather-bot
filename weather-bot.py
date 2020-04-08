26db6d1f5adcaf36187cf3050282a98b6abe4795
karvozavr <karvozavr@gmail.com>
2020-04-08 17:36:26 +0300

    Add inline location query

--- weather-bot.py
+++ weather-bot.py
@@ -2,6 +2,7 @@ import os
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 
 API_TOKEN = os.getenv('TELEGRAM_API_TOKEN') 
 # PROXY_URL = os.getenv('TELEGRAM_PROXY_URL')
@@ -15,6 +16,16 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start', 'help'])
 async def start(message: types.Message):
     await message.reply("Погода в вашем городе.", reply=False)
+
+@dp.inline_handler()
+async def inline_handler(query: InlineQuery):
+    location = query.location
+    result_id: str = f'{location.latitude}, {location.longitude}'
+    item = InlineQueryResultArticle(
+        id=result_id,
+        title=f'Result {result_id!r}',
+        input_message_content=InputTextMessageContent("Aaaaaaa"),
+    )
 
 
 if __name__ == '__main__':
